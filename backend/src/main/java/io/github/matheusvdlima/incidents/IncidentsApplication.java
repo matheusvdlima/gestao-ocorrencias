223cd1dad665eca72a421b4487c8d8f397b1a031
Matheus <matheuslima1403@gmail.com>
2025-08-22 19:31:18 -0300

    Estrutura inicial do backend. Criação e configuração do banco.

---
+++ backend/src/main/java/io/github/matheusvdlima/incidents/IncidentsApplication.java
@@ -0,0 +1,13 @@
+package io.github.matheusvdlima.incidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentsApplication.class, args);
+	}
+
+}
